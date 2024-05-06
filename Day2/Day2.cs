9d098970c7a449a5d7d47f46233fb3084de26f30
Bruno Oliveira <bruoli3@gmail.com>
2024-05-06 20:41:49 -0300

    Day2

---
+++ Day2/Day2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day2
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticOperators(args);
+
+        }
+
+        /*
+         * Arithmetic Operators
+         */
+        static void ArithmeticOperators(string[] args)
+        {
+            int n1 = 3 + 4 * 2;
+            Console.WriteLine(n1); // 11
+        }
+    }
+}
